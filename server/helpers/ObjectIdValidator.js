2b8655a93eb3b15a480fc798a3827c612084806e
mkofdwu <mkofdwu@gmail.com>
2022-05-12 11:35:17 +0800

    remove unnecessary server folder

--- server/helpers/ObjectIdValidator.js
+++
@@ -1,6 +0,0 @@
-const ObjectId = require('mongoose').Types.ObjectId;
-
-function isValidObjectId(id) {
-  return ObjectId.isValid(id);
-}
-module.exports = {isValidObjectId};
--- server/middleware/heroku-redirect-https.js
+++
@@ -1,7 +0,0 @@
-const herokuRedirectHttps = (req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https')
-    res.redirect(`https://${req.header('host')}${req.url}`);
-  else next();
-};
-
-module.exports = herokuRedirectHttps;
