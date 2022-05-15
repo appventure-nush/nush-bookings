2c1b930d4e0974b34ff9d535ada0e94f965769ac
mkofdwu <mkofdwu@gmail.com>
2022-05-15 21:03:03 +0800

    fix format issue in timingcard

--- src/utils/formatTiming.js
+++ src/utils/formatTiming.js
@@ -1,11 +1,10 @@
-export function formatTiming(timing) {
+export function formatTiming(timing, addTimeOfDay = true) {
   const timeOfDay = timing >= 1200 ? 'pm' : 'am';
   timing = timing % 1200;
   return (
     (Math.floor(timing / 100) || 12) +
     '.' +
     ((timing % 100) + '').padStart(2, '0') +
-    ' ' +
-    timeOfDay
+    (addTimeOfDay ? ' ' + timeOfDay : '')
   );
 }
