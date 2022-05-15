0ebfc53dd72c2a493e8658843975ea3b1f1456e9
mkofdwu <mkofdwu@gmail.com>
2022-05-15 20:30:24 +0800

    clean up dbservice.js

--- src/api/DbService.js
+++ src/api/DbService.js
@@ -1,9 +1,17 @@
-// import client from './client';
-
-const firebaseApp = require('firebase/app');
-const firestore = require('firebase/firestore');
-// import { initializeApp } from "firebase/app";
-// import { collection, getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import {
+  collection,
+  getFirestore,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  setDoc,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCzGf6_C5oxBu84nozg68tYD7mlwXbKUc4',
@@ -15,24 +23,12 @@ const firebaseConfig = {
   measurementId: 'G-DR3Y1SZP1L',
 };
 
-// Initialize Firebase
-const app = firebaseApp.initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = firestore.getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default {
-  /* GET list of tours */
-  // router.get('/', async function (req, res) {
   async getAllTours() {
-    // Initialize Firebase
-    const app = firebaseApp.initializeApp(firebaseConfig);
-
-    // Initialize Cloud Firestore and get a reference to the service
-    const db = firestore.getFirestore(app);
-    const querySnapshot = await firestore.getDocs(
-      firestore.collection(db, 'tours')
-    );
+    const querySnapshot = await getDocs(collection(db, 'tours'));
     var compliedData = [];
     querySnapshot.forEach((doc) => {
       console.log(`${doc.id} => ${doc.data()}`);
@@ -41,34 +37,25 @@ export default {
       );
     });
 
-    // res.send(compliedData.slice(start=1))
     return compliedData;
   },
 
-  /* GET a specific tour by ID*/
-  // router.get('/:id', async function (req, res) {
   async getTour(tour_id) {
-    const docRef = firestore.doc(db, 'tours', tour_id);
-    const docSnap = await firestore.getDoc(docRef);
+    const docRef = doc(db, 'tours', tour_id);
+    const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      console.log('Document data:', docSnap.data());
-      // res.send(docSnap.data());
       return docSnap.data();
-      // doc.data() will be undefined in this case
     } else {
+      // doc.data() will be undefined in this case
       console.log('No such document!');
-      // res.send("No such tour!");
     }
   },
 
-  /* GET tours by TIMING */
-  // router.get('/time/:time', async function (req, res) {
   async getTourByTime(time) {
-    const docRef = firestore.collection(db, 'tours');
-    // res.send(compliedData.slice(start=1));
-    const q = firestore.query(docRef, firestore.where('time', '==', time));
-    const querySnapshot = await firestore.getDocs(q);
+    const docRef = collection(db, 'tours');
+    const q = query(docRef, where('time', '==', time));
+    const querySnapshot = await getDocs(q);
 
     var compliedData = [];
     querySnapshot.forEach((doc) => {
@@ -78,21 +65,19 @@ export default {
       );
     });
 
-    // res.send(compliedData.slice(start=1))
     return compliedData;
   },
 
-  /* GET Tour ID by Participant */
   async getTourbyParticipant(phone_no, pax) {
-    const docRef = firestore.collection(db, 'tours');
-    const q = firestore.query(
+    const docRef = collection(db, 'tours');
+    const q = query(
       docRef,
-      firestore.where('participants', 'arrayContains', {
+      where('participants', 'arrayContains', {
         phone_no: phone_no,
         pax: pax,
       })
     );
-    const querySnapshot = await firestore.getDocs(q);
+    const querySnapshot = await getDocs(q);
 
     var compliedData = [];
     querySnapshot.forEach((doc) => {
@@ -102,40 +87,31 @@ export default {
       );
     });
 
-    // res.send(compliedData.slice(start=1))
     return compliedData;
   },
 
-  /* PUT new participant */
-  // router.put('/:id', async function (req, res) {
   async addParticipant(tour_id, phone_no, pax) {
-    const docRef = firestore.doc(db, 'tours', tour_id);
+    const docRef = doc(db, 'tours', tour_id);
     var newParticipant = {
       phone_no: phone_no,
       pax: pax,
     };
 
-    await firestore.updateDoc(docRef, {
-      participants: firestore.arrayUnion(newParticipant),
+    await updateDoc(docRef, {
+      participants: arrayUnion(newParticipant),
     });
-
-    // res.sendStatus(201);
   },
 
-  /* DELETE a participant */
-  // router.delete('/:id', async function (req, res) {
   async deleteParticipant(tour_id, phone_no, pax) {
-    const docRef = firestore.doc(db, 'tours', tour_id);
+    const docRef = doc(db, 'tours', tour_id);
     var participant = {
       phone_no: phone_no,
       pax: pax,
     };
 
-    await firestore.updateDoc(docRef, {
-      participants: firestore.arrayRemove(participant),
+    await updateDoc(docRef, {
+      participants: arrayRemove(participant),
     });
-
-    // res.sendStatus(200);
   },
 
   async reinitialiseTours() {
@@ -151,7 +127,7 @@ export default {
     for (let i = 0; i < count; i++) {
       for (let j = 0; j < routeCount; j++) {
         const tour_id = morningTimings[i] + '_' + morningRoutes[j];
-        await firestore.setDoc(firestore.doc(db, 'tours', tour_id), {
+        await setDoc(doc(db, 'tours', tour_id), {
           time: morningTimings[i],
           tour_id: tour_id,
           route: morningRoutes[j],
@@ -173,7 +149,7 @@ export default {
     for (let i = 0; i < afternoonCount; i++) {
       for (let j = 0; j < aftRouteCount; j++) {
         const tour_id = afternoonTimings[i] + '_' + afternoonRoutes[j];
-        await firestore.setDoc(firestore.doc(db, 'tours', tour_id), {
+        await setDoc(doc(db, 'tours', tour_id), {
           time: afternoonTimings[i],
           tour_id: tour_id,
           route: afternoonRoutes[j],
